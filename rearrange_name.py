b7a44744ac93c7903a6d5049edd1c513daf9f91f
Jason Schulberg <jason.schulberg93@gmail.com>
2020-04-08 16:01:16 -0400

    added the actual code for rearrange

---
+++ rearrange_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import re
+
+
+
+def rearrange_name(name):
+    result = re.search(r'^([\w .]*), ([\w .]*)$', name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+print(rearrange_name('bond, joey'))
+
+def separate_name(name):
+    result = re.search
